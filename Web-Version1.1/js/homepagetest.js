525e37c4536db2cd4744a630583cc48d54a1c05a
zhangyongming <zym1115718204@163.com>
2016-04-05 13:42:10 +0800

    FengSensor Web Version1.1

---
+++ Web-Version1.1/js/homepagetest.js
@@ -0,0 +1,70 @@
+$(document)
+  .ready(function() {
+    var
+      changeSides = function() {
+        $('.ui.shape')
+          .eq(0)
+            .shape('flip over')
+            .end()
+          .eq(1)
+            .shape('flip over')
+            .end()
+          .eq(2)
+            .shape('flip over')
+            .end()
+          .eq(3)
+            .shape('flip over')
+            .end()
+          .eq(4)
+            .shape('flip over')
+            .end()
+          .eq(5)
+            .shape('flip over')
+            .end()
+          .eq(6)
+            .shape('flip over')
+            .end()
+			
+        ;
+      },
+      validationRules = {
+        firstName: {
+          identifier  : 'email',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Please enter an e-mail'
+            },
+            {
+              type   : 'email',
+              prompt : 'Please enter a valid e-mail'
+            }
+          ]
+        }
+      }
+    ;
+
+    $('.ui.dropdown')
+      .dropdown({
+        on: 'hover'
+      })
+    ;
+
+    $('.ui.form')
+      .form(validationRules, {
+        on: 'blur'
+      })
+    ;
+
+    $('.masthead .information')
+      .transition('scale in')
+    ;
+
+    setInterval(changeSides, 3000);
+	
+	$('.flexslider').flexslider({
+		directionNav: true,
+		pauseOnAction: false
+	});
+		});
+	
